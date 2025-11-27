b0a168e66d2d636f671f450501c0b053727f79bf
TomLe1227 <2005thomasle@gmail.com>
2025-11-26 20:50:56 -0800

    Calculated Divisor 5000

--- Clock_Divider.sv
+++ Clock_Divider.sv
@@ -1,8 +1,8 @@
 module Clock_Divider #(
     // 1 MHz  = 1,000 kHz
     // 100 MHz  = 100,000 kHz
-    // 100 MHz  = 1 kHz * 100,000 * 100
-    parameter DIVISOR = 10000000  // Division factor to get from 100 MHz to 1 kHz
+    // 100 MHz  = 1 kHz * 100,000
+    parameter DIVISOR = 50000  // Division factor to get from 100 MHz to 1 kHz. Divide by 2 to get 50% duty cycle
 ) (
     input logic clk_in,    // Input clock (100 MHz)
     input logic rst,       // Active high reset
