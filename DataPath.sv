d6cb9ee8eadfbbe9578bfcd4fa0914563669443c
TomLe1227 <2005thomasle@gmail.com>
2025-12-24 01:01:04 -0800

    fixed mulitple drivers on signals

--- DataPath.sv
+++ DataPath.sv
@@ -1,4 +1,6 @@
 // RESET -> SET -> GO -> SCORE
+// Biggest problems: 
+// 1. Multiple Drivers on signals (synthesis error)
 module DataPath (
     input logic clk,
     input logic rst,
@@ -16,6 +18,21 @@ Clock_Divider DUT1 (
 );
 // Stopwatch
 logic [$clog2(10000)-1:0] elapsed_time;
+logic [$clog2(10000)-1:0] displayValue;
+logic [$clog2(10000)-1:0] final_time;
+always_ff @(posedge clk or posedge rst) begin
+  if (rst) final_time <= '0;
+  else if (current_state == GO && start_stop_btn)
+    final_time <= elapsed_time;
+end
+always_comb begin
+  case (current_state)
+    RESET: displayValue = '0;
+    SET: displayValue = '0;
+    GO: displayValue = elapsed_time;
+    SCORE: displayValue = final_time;
+  endcase
+end
 logic startWatch;
 Stopwatch DUT2 (
     .clk(clk),
@@ -35,7 +52,7 @@ RandomGen DUT3 (
 // Display Pipeline
 logic [6:0] display_out [3:0] // 4-digit 7-segment display output 
 binary_to_ssd DUT4 (
-    .binary_in(elapsed_time),
+    .binary_in(displayValue),
     .display_out(display_out)
 );
 basys_ssd DUT5 (
@@ -46,23 +63,31 @@ basys_ssd DUT5 (
     .seg(seg)
 );
 
+// FSM Implementation
+// Delay Counter in 1kHz clock domain
+
 logic [$clog2(10000)-1:0] delay_time;
 statetype next_state;
 statetype current_state;
-// FSM Implementation
-logic [1:0] state;
-always_ff @(posedge clk or posedge rst) begin 
+
+always_ff @(posedge clkdiv or posedge rst) begin 
     if (rst) begin
-        current_state <= RESET;
         delay_time <= 0;
     end else begin
    // Delay counter runs ONLY in SET
     if (current_state == SET) begin
         delay_time <= delay_time + 1;
     end else begin
-        current_state <= next_state; // Update state in sequential driver, every clock edge
         delay_time <= 0; // Reset delay_time when not in SET
     end
+  end
+end
+// State Register in 100Mhz clk domain
+always_ff @(posedge clk or posedge rst) begin 
+    if (rst) begin
+        current_state <= RESET;
+    end else begin
+    current_state <= next_state; // Update state in sequential driver, every clock edge
   end
 end
 
@@ -81,7 +106,7 @@ always_comb begin
         end
         SET: begin 
             // Logic for SET state
-            if (delay_time >= random_number*1000) next_state = GO; 
+            if (delay_time >= 500+random_number*10) next_state = GO; 
         end
         GO: begin 
             // Logic for GO state
